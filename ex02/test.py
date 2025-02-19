16d2cf725176769f5f8024f41a3744515cbd9e39
chrhu <chrhu@student.42.fr>
2025-02-19 13:40:46 +0100

    add tesst

--- ex02/test.py
+++ ex02/test.py
@@ -66,55 +66,63 @@ if __name__ == "__main__" :
 
     
     print("\n ===== Try T() =====")
-    # try:
-    #     # Example 1:
-    #     v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
-    #     print(v1.shape)
-    #     # Expected output:
-    #     (4,1)
-    #     print(v1.T())
-    #     # Expected output:
-    #     # Vector([[0.0, 1.0, 2.0, 3.0]])
-    #     print(v1.T().shape)
-    #     # Expected output:
-    #     # (1,4)
-    #     # Example 2:
-    #     v2 = Vector([[0.0, 1.0, 2.0, 3.0]])
-    #     print(v2.shape)
-    #     # Expected output:
-    #     # (1,4)
-    #     print(v2.T())
-    #     # Expected output:
-    #     # Vector([[0.0], [1.0], [2.0], [3.0]])
-    #     print(v2.T().shape)
-    #     # Expected output:
-    #     # (4,1)
-    # except Exception as e:
-    #     print(e)
+    try:
+        # Example 1:
+        v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+        
+        print(v1.shape)
+        # Expected output:
+        # (4,1)
 
-    print("\n ===== Try .dot and __str__ __repr__  =====")
-    # try :
-    #     # Example 1:
-    #     v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
-    #     v2 = Vector([[2.0], [1.5], [2.25], [4.0]])
+        print(v1.T())
+        # Expected output:
+        # Vector([[0.0, 1.0, 2.0, 3.0]])
 
-    #     print(v1.dot(v2))
-    #     # Expected output:
-    #     # 18.0
+        print(v1.T().shape)
+        # Expected output:
+        # (1,4)
 
-    #     v3 = Vector([[1.0, 3.0]])
-    #     v4 = Vector([[2.0, 4.0]])
 
-    #     print(v3.dot(v4))
-    #     # Expected output:
-    #     # 14.0
+        # Example 2:
+        v2 = Vector([[0.0, 1.0, 2.0, 3.0]])
 
-    #     v1
-    #     # Expected output: to see what __repr__() should do
-    #     # [[0.0, 1.0, 2.0, 3.0]]
+        print(v2.shape)
+        # Expected output:
+        # (1,4)
 
-    #     print(v1)
-    #     # Expected output: to see what __str__() should do
-    #     # [[0.0, 1.0, 2.0, 3.0]]
-    # except Exception as e:
-    #     print(e)
+        print(v2.T())
+        # Expected output:
+        # Vector([[0.0], [1.0], [2.0], [3.0]])
+
+        print(v2.T().shape)
+        # Expected output:
+        # (4,1)
+    except Exception as e:
+        print(e)
+
+    print("\n ===== Try .dot and  __repr__  =====")
+    try :
+        # Example 1:
+        v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+        v2 = Vector([[2.0], [1.5], [2.25], [4.0]])
+
+        print(v1.dot(v2))
+        # Expected output:
+        # 18.0
+
+        v3 = Vector([[1.0, 3.0]])
+        v4 = Vector([[2.0, 4.0]])
+
+        print(v3.dot(v4))
+        # Expected output:
+        # 14.0
+
+        v1
+        # Expected output: to see what __repr__() should do
+        # [[0.0, 1.0, 2.0, 3.0]]
+
+        print(v1)
+        # Expected output: to see what __str__() should do
+        # [[0.0, 1.0, 2.0, 3.0]]
+    except Exception as e:
+        print(e)
